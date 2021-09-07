c9590be5ba2945612cc5e6d6cf52833b5d2d4a80
samrajm <samrajm1708@gmail.com>
2021-09-07 15:31:16 +0530

    committed

---
+++ src/org/print/HelloWorld.java
@@ -0,0 +1,21 @@
+package org.print;
+
+public  class HelloWorld {
+	
+	public static void sysout() {
+		System.out.println("Hello World");
+
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+	HelloWorld  s = new HelloWorld();
+	s.sysout();
+	
+
+
+	
+	}
+}
